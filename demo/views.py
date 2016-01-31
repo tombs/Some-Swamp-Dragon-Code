535619eefb8e7597624537bfdd5e2776351ca63f
root <root@doublespace.ph>
2016-02-01 06:18:53 +0800

    - added functional view for contacts

--- demo/views.py
+++ demo/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.base import View
 from .models import Notification
-
+from django.conf import settings
+from django.shortcuts import get_object_or_404, redirect
+from django.http import HttpResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 class Notifications(ListView):
     model = Notification
@@ -44,4 +49,27 @@ class Contact(ListView):
     def get_queryset(self):
         return self.model.objects.filter(status='new').order_by('created')[:1]
 
+@csrf_exempt
+def contactsent(request, *args, **kwargs):
+    from django.core.mail import send_mail
+      
+    if request.method == 'POST':
+ 
+        name = request.POST.get('name','')
+        phone = request.POST.get('phone','')
+        email = request.POST.get('email','') 
+        message = request.POST.get('message','')    
 
+        body = str(email) + "\n\n" + str(phone) + "\n\n\n\n" + str(message)
+        title = 'DoubleSpace PH Contact Request from: ' + str(name)
+
+        send_mail(title, body, settings.EMAIL_HOST_USER, settings.EMAIL_CONTACT_RECIPIENTS, fail_silently=False)        
+        response_data = {}
+        return HttpResponse(json.dumps(response_data),content_type="application/json")
+
+
+    else:
+        return HttpResponse(
+            json.dumps({"nothing to see": "this isn't happening"}),
+            content_type="application/json"
+        )
